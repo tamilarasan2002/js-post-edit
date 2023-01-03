2ff80576e4e1cb7208d655ade1f3283a41ab0680
D.Tamilarasan <abidm26791@gmail.com>
2023-01-03 02:25:15 -0800

    js

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-let container=document.querySelector(".container");
+let row=document.querySelector(".row");
 let email=document.querySelector(".emailid");
 let save=document.querySelector(".saveid");
+
 save.addEventListener("click",function(){
+
+    var a={};
+        a["emails"]=email.value;
     let content=document.createElement("div");
-    container.appendChild(content);
+    row.appendChild(content);
     content.classList.add("content");
-
-    let a={};
-    a["emails"]=email.value;
-   
-
-    
   
     let icon1=document.createElement("span");
-    icon1.setAttribute("class","fa fa-minus");
+    icon1.setAttribute("class","fa fa-trash");
     content.appendChild(icon1);
+
     icon1.addEventListener("click",function(){
     this.parentNode.remove();
     }); 
 
     para=document.createElement("p");
+    para.classList.add("para")
     content.appendChild(para);
     para.textContent=email.value;
     email.value="";
     
     let icon2=document.createElement("span");
-    icon2.setAttribute("class","fa fa-plus");
+    icon2.setAttribute("class","fa fa-edit");
     content.appendChild(icon2);
     icon2.addEventListener("click",function(){
-    email.value=this.parentNode.emails
+    email.value=this.previousElementSibling.innerText
+    this.parentNode.remove();
     });
 
 
